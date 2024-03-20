7f82a576404f5b057462b2a93e0546ee51cb8772
MauriJC <xaviermauricio33@gmail.com>
2024-03-19 22:55:12 -0300

    updated start script to legacy

--- src/components/Diagnosticos/ListadoDiagnosticos/index.js
+++ src/components/Diagnosticos/ListadoDiagnosticos/index.js
@@ -1,3 +1,4 @@
+
 import React,{useEffect, useState} from 'react'
 import { Link } from 'react-router-dom';
 import api from '../../../apis'
@@ -13,7 +14,7 @@ const ListadoDiagnosticos = () => {
 
     //console.log(matricula, nroAfiliado)
 
-    
+    setinfoPaciente({});
 
     useEffect(()=>{
          
